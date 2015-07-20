9b9030e24075499dbdf91660d704d06c85b67741
Chris Hatton <christopher.hatton@digital.landregistry.gov.uk>
2015-07-20 10:47:35 +0100

    added sed expresion

--- monitoring.bash
+++ monitoring.bash
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-logging_file="/home/filesystem_usage"
+logging_file="filesystem_usage"
 
 collect_filesystem_info ()
 # Collect the file system sizes
 {
   current_date=`date | tr -d 'n' `
-  filesystem_size=`df -h | grep 'mapper' |  awk ' {print $5} '`
+  filesystem_size=`df -h | grep 'mapper\|none' |  awk ' {print $5} '`
   filesystem_size_element=( $filesystem_size )
   echo $current_date$(printf ",%s" "${filesystem_size_element[@]//%}") >> $logging_file
 }
@@ -15,10 +15,10 @@ create_header ()
 # Create header for the file if required
 {
   header_info=`df -h | grep 'mapper\|none' | awk ' {print $1 } '`
-  header_info_element=($header_info)
-  if [ ! -f "$logging_file" ]; then
-      echo $(printf "date,%s" "${header_info_element[@]}") >> $logging_file
-  fi
+  header=`echo $header_info | sed 's/ /,/'`
+  if [ ! -f $logging_file ]; then
+      echo date,${header} >> $logging_file
+ fi
 }
 
 create_header
